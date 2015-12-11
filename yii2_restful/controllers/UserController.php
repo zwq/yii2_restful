415f1e086185af49f38fb21bd8ed369ad6ab5ef5
unknown <276256472@qq.com>
2015-12-11 18:22:22 +0800

    first restful

---
+++ yii2_restful/controllers/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\rest\ActiveController;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\QueryParamAuth;
+
+class UserController extends ActiveController
+{
+    public $modelClass = 'app\models\User';
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvelope' => 'items',
+    ];
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                QueryParamAuth::className(),
+            ],
+        ];
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        // 注销系统自带的实现方法
+        unset($actions['index'], $actions['update'], $actions['create'], $actions['delete'], $actions['view']);
+        return $actions;
+    }
+
+    public function actionIndex()
+    {
+        $modelClass = $this->modelClass;
+        $query = $modelClass::find();
+        return new ActiveDataProvider([
+            'query' => $query,
+        ]);
+    }
+
+    public function actionCreate()
+    {
+        $model = new $this->modelClass();
+        $model->load(Yii::$app->getRequest()->getBodyParams(), '');
+        if (!$model->save()) {
+            return array_values($model->getFirstErrors())[0];
+        }
+        return $model;
+    }
+
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        $model->load(Yii::$app->getRequest()->getBodyParams(), '');
+        if (!$model->save()) {
+            return array_values($model->getFirstErrors())[0];
+        }
+        return $model;
+    }
+
+    public function actionDelete($id)
+    {
+        return $this->findModel($id)->delete();
+    }
+
+    public function actionView($id)
+    {
+        return $this->findModel($id);
+    }
+
+    protected function findModel($id)
+    {
+        $modelClass = $this->modelClass;
+        if (($model = $modelClass::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+
+}
